c8eb7b89690bc2befcd359c1654ff63c5c39ccf7
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-07-19 17:34:50 -0300

    Alterações no Javascript

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
-function exibir() {
+function ordemCrescente(n1,n2,n3) {
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    n3 = parseInt(n2);
 
-     n1 = parseInt(n1);
-     n2 = parseInt(n2);
-     n3 = parseInt(n3);
-     ordem1 = 0;
-     ordem2 = 0;
-     ordem3 = 0;
+    if (n1 <=2 && n1 <= n3) {
+        document.write('A ordem 1 é: ' + n1 +'<br>');
+    }
+    if( n2 <= n3){
+        document.write('A ordem 2 é: ' + n2 +'<br>');
+    } 
+    
+   
 
-
-
-    if (n1 < n2 && n1 < n3){
-       document.write(' ' + ordem1 == n1 +'<br> ');
-   }
-   if (n2 < n3) {
-       document.write('' + ordem2 == n2 + '<br>');
-       document.write(' ' + ordem3 == n3 + '<br>')
-   } else{
-       document.write(' ' + ordem2 == n3 + '<br>');
-   }
-
-   document.write(' A ordem é: ' + exibir() + '');
+   
 
 }
 
-var n1 = prompt('Digite o valor de A');
-var n2 = prompt('Digite o valor de B');
-var n3 = prompt('Digite o valor de C');
+var n1 =prompt('Digite o valor de A');
+var n2 =prompt('Digite o valor de B');
+var n3 =prompt('Digite o valor de C');
 
-exibir();
+ordemCrescente(n1,n2,n3);
