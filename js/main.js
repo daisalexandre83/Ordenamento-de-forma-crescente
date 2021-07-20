e11d120b39ce81cf31d10de9e54c01ccf0dfe976
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-07-19 22:35:04 -0300

    Alterações no JS

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
-function ordemCrescente(n1,n2,n3) {
-    n1 = parseInt(n1);
-    n2 = parseInt(n2);
-    n3 = parseInt(n2);
+function ordemCrescente(n1,n2,n3){
+    n1 = parseFloat(n1);
+    n2= parseFloat(n2);
+    n3=parseFloat(n3);
+    ordem1;
+    ordem2;
+    ordem3;
 
-    if (n1 <=2 && n1 <= n3) {
-        document.write('A ordem 1 é: ' + n1 +'<br>');
+    if(n1 <= n2 && n1 <= n3){
+        ordem1 = n1;
     }
-    if( n2 <= n3){
-        document.write('A ordem 2 é: ' + n2 +'<br>');
-    } 
-    
-   
+    if(n2 <= n3){
+        ordem2 = n2;
+        ordem3 = n3;
+    }else{
+        ordem2 = n3;
+        ordem3 = n2;
+    }
 
-   
+    if(n2 <= n3 && n2 <= num1){
+        ordem1 =n2;
+    }
+    if(n1 <= n3){
+        ordem2 = n1;
+        ordem3 = n3;
+    } else{
+        ordem2 = n3;
+        ordem3 = n1;
+    }
+    
+    if( n3 <= n1 && n3 <= n2){
+        ordem1 = n3;
+    }
+    if(n2 <= n1){
+        
+    }
 
 }
 
-var n1 =prompt('Digite o valor de A');
-var n2 =prompt('Digite o valor de B');
-var n3 =prompt('Digite o valor de C');
+var n1 = prompt('Digite o número A');
+var n2 = prompt('Digite o número B');
+var n3 = prompt('Digite o número C');
 
 ordemCrescente(n1,n2,n3);
